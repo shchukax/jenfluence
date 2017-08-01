4cdd32f221447cfa8e9e029e96e99c3417f3313a
Martin Walter <martin.walter@sprengnetter.de>
2017-08-01 09:09:48 +0200

    Added serialVersionUID.

--- src/main/java/de/sprengnetter/jenkins/plugins/jenfluence/step/By.java
+++ src/main/java/de/sprengnetter/jenkins/plugins/jenfluence/step/By.java
@@ -1,27 +1,30 @@
 package de.sprengnetter.jenkins.plugins.jenfluence.step;
 
-import org.kohsuke.stapler.DataBoundConstructor;
-
 import java.io.Serializable;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * @author Oliver Breitenbach
  * @version 1.0.0
- * Model for the specification of a parent page in Confluence.
+ *          Model for the specification of a parent page in Confluence.
  */
 public class By implements Serializable {
+
+    private static final long serialVersionUID = 3518103351909578986L;
 
     // TITLE, ID or NONE
     private String value;
 
-    //The value of the parent identifier (E.g The title or the ID of a page)
+    // The value of the parent identifier (E.g The title or the ID of a page)
     private String parentIdentifier;
 
     /**
      * Constructor which takes the values of this model.
      *
-     * @param value            The value.
-     * @param parentIdentifier The parent identifier (ID or title of a page).
+     * @param value
+     *        The value.
+     * @param parentIdentifier
+     *        The parent identifier (ID or title of a page).
      */
     @DataBoundConstructor
     public By(final String value, final String parentIdentifier) {
